f44299c558cb9c3a143d1a3d3b454f85fb44f2c2
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2019-02-26 17:28:46 +0900

    Add 5.6 allocation

---
+++ src/5-6-allocation.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+typedef long long llong;
+
+class MinimumCapacitySolver {
+
+public:
+    static const int MAX = 100000;
+
+    MinimumCapacitySolver (int cCnt, int tCnt, llong weights[]) {
+        for (int i = 0; i < cCnt; i++) {
+            cargoWeights[i] = weights[i];
+        }
+        cargoCnt = cCnt;
+        trackCnt = tCnt;
+    }
+
+    int check(llong trackCapacity) {
+        auto i = 0;
+        for (int j = 0; j < trackCnt; j++) {
+            llong s = 0;
+            while(s + cargoWeights[i] <= trackCapacity) {
+               s += cargoWeights[i];
+               i++;
+               if (i == cargoCnt) {
+                   return cargoCnt;
+               }
+            }
+        }
+        return i;
+    }
+
+    int solve() {
+        llong left = 0;
+        llong right = MAX * 10000;
+        llong mid;
+
+        while(right - left > 1) {
+            mid = (left + right) / 2;
+            auto v = check(mid);
+        
+            if (v >= cargoCnt) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+
+        return right;
+    }
+
+private: 
+    llong cargoWeights[MAX];
+    int cargoCnt;
+    int trackCnt;
+};
+
+int main() {
+    int cargoCnt, trackCnt;
+    llong cargoWeights[MinimumCapacitySolver::MAX];
+    cin >> cargoCnt >> trackCnt;
+
+    for (int i = 0; i < cargoCnt; i++) {
+        cin >> cargoWeights[i];
+    }   
+    auto *solver = new MinimumCapacitySolver(cargoCnt, trackCnt, cargoWeights);
+
+    llong ans = solver->solve();
+    cout << ans << endl;
+}
